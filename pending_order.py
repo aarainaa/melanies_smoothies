5df16ec6f21f856976376c42c87fededbf26aa6f
aarainaa <nidhisparihar@gmail.com>
2025-02-28 14:00:59 +0530

    Update pending_order.py

--- pending_order.py
+++ pending_order.py
@@ -1,32 +1,41 @@
-#Import python packages
+# Import python packages
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col, when_matched
+from snowflake.snowpark.functions import col,when_matched
+helpful_links = [
+    "https://docs.streamlit.io",
+    "https://docs.snowflake.com/en/developer-guide/streamlit/about-streamlit",
+    "https://github.com/Snowflake-Labs/snowflake-demo-streamlit",
+    "https://docs.snowflake.com/en/release-notes/streamlit-in-snowflake"
+]
 
 # Write directly to the app
 st.title(":cup_with_straw: Pending Smoothie orders :cup_with_straw:")
-st.write(f"""Orders that needs to be filled""")
-
+st.write(
+    f"""Orders that needs to be filled
+    """
+)
 session = get_active_session()
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
-editable_df = st.data_editor(my_dataframe)
-submitted= st.button('Submit')
 if my_dataframe:
- 
-  
-  if submitted:
+ editable_df = st.data_editor(my_dataframe)
+
+ submitted= st.button('Submit')
+ if submitted:
    
     og_dataset = session.table("smoothies.public.orders")
     edited_dataset = session.create_dataframe(editable_df)
     
     try: 
-        og_dataset.merge(edited_dataset
+      
+     og_dataset.merge(edited_dataset
                      , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
                      , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
                     )
 
-        st.success('Order updated', icon = '👍')
+     st.success('Order updated', icon = '👍')
+    
     except:
-        st.write('Something went wrong.')
+      st.write('Something went wrong.')
 else:
     st.success('No orders to fill', icon= '👍')
